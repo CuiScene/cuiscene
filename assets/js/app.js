0e5fb476f8ecc3035d722ee09dc241d1cce60302
Youssef <yhamid1982@gmail.com>
2018-11-12 22:42:33 -0500

    Internal API Edits
    DB Get & Post requests connecting.
    User form submitting, but not returning id...I believe due to validation issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,41 @@
+
 var zomatoCitySearch = function (city) {
-  var queryurl = "https://developers.zomato.com/api/v2.1/cities?q=" + city
+  var queryurl = 'https://developers.zomato.com/api/v2.1/cities?q=' + city
   $.ajax({
     url: queryurl,
-    type: "GET",
-    dataType: "json",
-    headers: { "user-key": "46a9d8c42a9a6217ff47dae868a48873" }
+    type: 'GET',
+    dataType: 'json',
+    headers: { 'user-key': '46a9d8c42a9a6217ff47dae868a48873' }
   })
     .then(function (response) {
       console.log(response)
-    });
+    })
 }
 
 $(function () {
-  $(".create-form").on("submit", function (event) {
+  $('.create-form').on('submit', function (event) {
     // Make sure to preventDefault on a submit event.
-    event.preventDefault();
+    event.preventDefault()
 
     var newUser = {
-      username_pk: $("#uname").val().trim(),
-      birthday: $("#bday").val().trim(),
-      restrictions: $("#restrict").val().trim(),
-      zipcode: $("#zip").val().trim(),
-    };
+      username_pk: $('#uname').val().trim(),
+      birthday: $('#bday').val().trim(),
+      restrictions: $('#restrict').val().trim(),
+      zipcode: $('#zip').val().trim()
+    }
     console.log(newUser)
     // Send the POST request.
-    $.ajax("/api/users", {
-      type: "POST",
+    $.ajax('/api/users', {
+      type: 'POST',
       data: newUser
     }).then(
       function () {
-        console.log("created new user");
+        console.log('created new user')
         // Reload the page to get the updated list
-        //location.reload();
+        // location.reload();
       }
-    );
-  });
-});
+    )
+  })
+})
 
-zomatoCitySearch("cleveland");
-
+zomatoCitySearch('cleveland')
